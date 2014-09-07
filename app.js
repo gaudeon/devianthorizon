73287487ff30ed126a47edfc7eb7058c6f9f8b9a
gaudeon <gaudeon@cpan.org>
2014-09-06 21:18:26 -0600

    handlebars added, jade removed

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// load routes
+var routes = require('./routes/index'); // routes for '/'
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var exphbs = require('express-handlebars');
+app.engine('html', exphbs({ extname: '.html', defaultLayout: 'main' }));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// setup routes
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
